b2f824c78a680545418e7402773ac82cbb8aa804
Le Tuan Anh <anhlt1983@gmail.com>
2016-06-15 13:38:55 +0700

    implement permission request and handler

--- app/src/main/java/com/packtpub/masteringopencvandroid/objectdetect/MainActivity.java
+++ app/src/main/java/com/packtpub/masteringopencvandroid/objectdetect/MainActivity.java
@@ -1,5 +1,6 @@
 package com.packtpub.masteringopencvandroid.objectdetect;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -7,11 +8,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
@@ -24,13 +27,23 @@ public class MainActivity extends Activity {
 
     public static final String TAG = "ObjectDetect::MainActivity";
 
-    private final int SELECT_PHOTO_1 = 1;
-    private final int SELECT_PHOTO_2 = 2;
+    // Request code definition
+    private final int SELECT_PHOTO_1    = 1;
+    private final int SELECT_PHOTO_2    = 2;
+    private final int READ_PERMISSION   = 1;
 
-    private boolean src1Selected = false, src2Selected = false;
-    Mat src1, src2;
+    private boolean src1Selected = false;
+    private boolean src2Selected = false;
 
     private ImageView ivImage1;
+
+    private TextView tvKeyPointsObject1;
+    private TextView tvKeyPointsObject2;
+    private TextView tvKeyPointsMatches;
+    private TextView tvTime;
+
+    private Mat src1;
+    private Mat src2;
 
     private boolean isAllowed = false;
 
@@ -39,6 +52,14 @@ public class MainActivity extends Activity {
         Log.d(TAG, "onCreate is called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Linking view component
+        ivImage1 = (ImageView)findViewById(R.id.ivImage1);
+        tvKeyPointsObject1 = (TextView) findViewById(R.id.tvKeyPointsObject1);
+        tvKeyPointsObject2 = (TextView) findViewById(R.id.tvKeyPointsObject2);
+        tvKeyPointsMatches = (TextView) findViewById(R.id.tvKeyPointsMatches);
     }
 
     @Override
@@ -78,6 +99,10 @@ public class MainActivity extends Activity {
 
             if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
                 Log.d(TAG, "Permission is not allowed. Request permission");
+
+                ActivityCompat.requestPermissions(this, new String[]
+                        {Manifest.permission.READ_EXTERNAL_STORAGE}, READ_PERMISSION);
+
             } else {
                 Log.d(TAG, "Permission is granted");
                 isAllowed = true;
@@ -97,7 +122,7 @@ public class MainActivity extends Activity {
                             final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                             //src1 = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC4);
                             ivImage1.setImageBitmap(selectedImage);
-                            Utils.bitmapToMat(selectedImage, src1);
+                            //Utils.bitmapToMat(selectedImage, src1);
                             src1Selected = true;
                         } catch (FileNotFoundException e) {
                             e.printStackTrace();
@@ -111,13 +136,28 @@ public class MainActivity extends Activity {
                             final InputStream imageStream = getContentResolver().openInputStream(imageUri);
                             final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                             //src2 = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC4);
-                            Utils.bitmapToMat(selectedImage, src2);
+                            //Utils.bitmapToMat(selectedImage, src2);
                             src2Selected = true;
                         } catch (FileNotFoundException e) {
                             e.printStackTrace();
                         }
                     }
                     break;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case READ_PERMISSION: {
+                // If request is cancelled, the result arrays are empty
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "Permission is granted");
+                    isAllowed = true;
+                } else {
+                    Log.d(TAG, "Permission is Not granted");
+                }
             }
         }
     }

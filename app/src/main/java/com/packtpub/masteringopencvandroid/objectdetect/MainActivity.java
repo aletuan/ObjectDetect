e579a93f0bcd0e392f798e76d4a1ed3d246a8b5e
Le Tuan Anh <anhlt1983@gmail.com>
2016-06-15 16:56:20 +0700

    implement SIFT, and draw output

--- app/src/main/java/com/packtpub/masteringopencvandroid/objectdetect/MainActivity.java
+++ app/src/main/java/com/packtpub/masteringopencvandroid/objectdetect/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -22,16 +23,26 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.DMatch;
 import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -54,6 +65,9 @@ public class MainActivity extends Activity {
     private int keypointsObject1;
     private int keypointsObject2;
     private int keypointMatches;
+
+    // Maximum number of match to draw
+    private final int MAX_MATCHES = 50;
 
     private TextView tvTime;
 
@@ -221,8 +235,8 @@ public class MainActivity extends Activity {
                     @Override
                     protected Bitmap doInBackground(Void... params) {
                         Log.d(TAG, "doInBackground begin");
-                        //return executeTask();
-                        return null;
+                        return executeTask();
+                        //return null;
                     }
 
                     @Override
@@ -263,6 +277,7 @@ public class MainActivity extends Activity {
                 detector = FeatureDetector.create(FeatureDetector.SIFT);
                 descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.SIFT);
                 descriptorMatcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_SL2);
+                Log.d(TAG, "Got descriptorMatcher");
                 break;
             default:
                 Log.d(TAG, "default case");
@@ -272,25 +287,86 @@ public class MainActivity extends Activity {
                 break;
         }
 
+        Log.d(TAG, "before detect");
+        Log.d(TAG, "image 1 " + src1.cols() + " / " + src1.rows());
+        Log.d(TAG, "image 2 " + src1.cols() + " / " + src1.rows());
+
+        // Get key points
+        Log.d(TAG, "start get key points");
         detector.detect(src2, keyPoints2);
         detector.detect(src1, keyPoints1);
 
-        Log.d(TAG, CvType.typeToString(src1.type())+" "+CvType.typeToString(src2.type()));
-        Log.d(TAG, keyPoints1.toArray().length+" key points");
-        Log.d(TAG, keyPoints2.toArray().length+" key points");
-        Log.d(TAG, "Detect");
+        Log.d(TAG, CvType.typeToString(src1.type()) + " " + CvType.typeToString(src2.type()));
+        Log.d(TAG, keyPoints1.toArray().length + " key points");
+        Log.d(TAG, keyPoints2.toArray().length + " key points");
 
         keypointsObject1 = keyPoints1.toArray().length;
         keypointsObject2 = keyPoints2.toArray().length;
 
+        Log.d(TAG, "start get key point descriptor");
         descriptorExtractor.compute(src1, keyPoints1, descriptors1);
         descriptorExtractor.compute(src2, keyPoints2, descriptors2);
 
+        Log.d(TAG, "matching two descriptor");
         descriptorMatcher.match(descriptors1, descriptors2, matches);
 
+        keypointMatches = matches.toArray().length;
 
-        return null;
+        Log.d(TAG, "number of match " + keypointMatches);
 
+        Collections.sort(matches.toList(), new Comparator<DMatch>() {
+            @Override
+            public int compare(DMatch o1, DMatch o2) {
+                if (o1.distance < o2.distance ) {
+                    return -1;
+                }
+                if (o1.distance > o2.distance) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+
+        List<DMatch> listOfDMatch = matches.toList();
+
+        if(listOfDMatch.size() > MAX_MATCHES){
+            matches.fromList(listOfDMatch.subList(0, MAX_MATCHES));
+        }
+
+        Log.d(TAG, "start to draw match");
+        Mat src3 = drawMatches(src1, keyPoints1, src2, keyPoints2, matches, false);
+
+        Bitmap image1 = Bitmap.createBitmap(src3.cols(), src3.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(src3, image1);
+        Imgproc.cvtColor(src3, src3, Imgproc.COLOR_BGR2RGB);
+
+        boolean bool = Highgui.imwrite(Environment.getExternalStorageDirectory()+"/Download/" + ACTION_MODE+".png", src3);
+        Log.d(TAG, bool + " " + Environment.getExternalStorageDirectory()+"/Download/" + ACTION_MODE + ".png");
+
+        return image1;
+
+    }
+
+    static Mat drawMatches(Mat img1, MatOfKeyPoint key1, Mat img2, MatOfKeyPoint key2, MatOfDMatch matches, boolean imageOnly){
+        //https://github.com/mustafaakin/image-matcher/tree/master/src/in/mustafaak/imagematcher
+        Mat out = new Mat();
+        Mat im1 = new Mat();
+        Mat im2 = new Mat();
+        Imgproc.cvtColor(img1, im1, Imgproc.COLOR_BGR2RGB);
+        Imgproc.cvtColor(img2, im2, Imgproc.COLOR_BGR2RGB);
+        if ( imageOnly){
+            MatOfDMatch emptyMatch = new MatOfDMatch();
+            MatOfKeyPoint emptyKey1 = new MatOfKeyPoint();
+            MatOfKeyPoint emptyKey2 = new MatOfKeyPoint();
+            Features2d.drawMatches(im1, emptyKey1, im2, emptyKey2, emptyMatch, out);
+        } else {
+            Features2d.drawMatches(im1, key1, im2, key2, matches, out);
+        }
+        Bitmap bmp = Bitmap.createBitmap(out.cols(), out.rows(), Bitmap.Config.ARGB_8888);
+        Imgproc.cvtColor(out, out, Imgproc.COLOR_BGR2RGB);
+        Core.putText(out, "FRAME", new Point(img1.width() / 2,30), Core.FONT_HERSHEY_PLAIN, 2, new Scalar(0,255,255),3);
+        Core.putText(out, "MATCHED", new Point(img1.width() + img2.width() / 2,30), Core.FONT_HERSHEY_PLAIN, 2, new Scalar(255,0,0),3);
+        return out;
     }
 
     @Override
